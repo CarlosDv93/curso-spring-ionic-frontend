4a800216a816c1eb8d9e1ece02ac0a5bb46695ce
Carlos David A Arcos <carlos9_cv@hotmail.com>
2018-12-20 14:26:29 -0200

    Carregando produtos de uma dada categoria

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,3 +1,4 @@
+import { ProdutoService } from './../../services/produto.service';
 import { ProdutoDTO } from './../../models/produto.dto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -18,22 +19,19 @@ export class ProdutosPage {
 
   items : ProdutoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public produtoService: ProdutoService) {
   }
 
   ionViewDidLoad() {
-    this.items = [
-      {
-        id:"1",
-        nome: "Mouse",
-        preco: 80.99
-      },
-      {
-        id:"2",
-        nome: "Computador",
-        preco: 100.00
-      }
-    ]
+    let categoriaId = this.navParams.get("categoria_id");
+    this.produtoService.findByCategoria(categoriaId)
+      .subscribe(response => {
+        this.items = response['content'];
+      }, error => {}
+    );
+      
   }
 
 }
