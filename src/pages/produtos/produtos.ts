bbd088a6b7e852f55155ce1317a92b57225a85e8
Carlos David A Arcos <carlos9_cv@hotmail.com>
2018-12-20 14:52:01 -0200

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,3 +1,4 @@
+import { API_CONFIG } from './../../config/api.config';
 import { ProdutoService } from './../../services/produto.service';
 import { ProdutoDTO } from './../../models/produto.dto';
 import { Component } from '@angular/core';
@@ -29,9 +30,21 @@ export class ProdutosPage {
     this.produtoService.findByCategoria(categoriaId)
       .subscribe(response => {
         this.items = response['content'];
+        this.loadImageUrls();
       }, error => {}
     );
       
+  }
+
+  loadImageUrls(){
+    for(var i = 0; i<this.items.length; i++){
+      let item = this.items[i];
+      this.produtoService.getSmallFileFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`
+        },
+        error => {});
+    }
   }
 
 }
--- src/services/produto.service.ts
+++ src/services/produto.service.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs/Rx';
 import { API_CONFIG } from './../config/api.config';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
@@ -9,5 +10,10 @@ export class ProdutoService {
 
     findByCategoria(categoria_id : string){
         return this.http.get(`${API_CONFIG.baseUrl}/produtos?categorias=${categoria_id}`);
+    }
+
+    getSmallFileFromBucket(id: string) : Observable<any>{
+        let url :string = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`;
+        return this.http.get(url, {responseType: "blob"});
     }
 }
