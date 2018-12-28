543386043d65a1ca8f46c719be9a63f6e2c66bc0
Carlos David A Arcos <carlos9_cv@hotmail.com>
2018-12-28 11:42:22 -0200

    Infinite scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -11,7 +11,8 @@ import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-an
 })
 export class ProdutosPage {
 
-  items : ProdutoDTO[];
+  items : ProdutoDTO[] = [];
+  page : number = 0;
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
@@ -26,10 +27,14 @@ export class ProdutosPage {
   loadData(){
     let categoriaId = this.navParams.get("categoria_id");
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoriaId)
+    this.produtoService.findByCategoria(categoriaId, this.page, 10)
       .subscribe(response => {
-        this.items = response['content'];
-        this.loadImageUrls();
+        let start = this.items.length;
+        this.items = this.items.concat(response['content']);
+        let end = this.items.length-1;
+        this.loadImageUrls(start, end);
+        console.log(this.page);
+        console.log(this.items);
         loader.dismiss();
       }, error => {
         loader.dismiss();
@@ -37,8 +42,8 @@ export class ProdutosPage {
     );
   }
 
-  loadImageUrls(){
-    for(var i = 0; i<this.items.length; i++){
+  loadImageUrls(start : number, end: number){
+    for(var i = start; i<= end; i++){
       let item = this.items[i];
       this.produtoService.getSmallFileFromBucket(item.id)
         .subscribe(response => {
@@ -61,9 +66,19 @@ export class ProdutosPage {
   }
 
   doRefresh(refresher) {
+    this.page = 0;
+    this.items = [];
     this.loadData();
     setTimeout(() => {
       refresher.complete();
+    }, 1000);
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+      infiniteScroll.complete();
     }, 1000);
   }
 
